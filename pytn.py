e170fec1782bdb0dcb1b43209560f8a30b1b20f1
tanveerlogavi <100829289+tanveerlogavi@users.noreply.github.com>
2022-03-03 13:20:34 +0530

    Add files via upload

---
+++ pytn.py
@@ -0,0 +1,101 @@
+
+
+'''print("hello world!")
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+  
+x = 5
+
+y = "HELLO!"
+
+print(x)
+print(y)
+
+myname="TaNvEeR LoGaVi"
+Myname="TaNvEeR LoGaVi"
+MYNAME="TaNvEeR LoGaVi"
+_MYNAME="TaNvEeR LoGaVi"
+_MY_name="TaNvEeR LoGaVi"
+
+print(myname)
+print(Myname)
+print(MYNAME)
+print(_MYNAME)
+print(type(_MY_name))
+
+
+
+
+cars = ["FORD", "BMW", "Ferrari"]
+x, y, z = cars
+
+print(x)
+print(y)
+print(z)
+
+x="AMAZING"
+def mycars():
+   global x
+   x="car"
+    
+mycars()
+print("ford is a " + x  )
+
+
+ab = range(10)
+
+#display x:
+print(ab)
+
+#display the data type of x:
+print(type(ab)) 
+
+x = ["apple", "banana", "cherry"]
+
+print(x)
+
+print(type(x))
+
+a = ("apple", "banana", "cherry")
+print(a)
+print(type(a))
+
+b = {"apple", "banana", "cherry"}
+print(b)
+print(type(b))
+
+c = {"apple": "banana", "cherry":400}
+print(c)
+print(type(c))
+
+#boolens type
+a=5>3
+x="True"
+print(a)
+print(type(a))
+
+import random
+
+print(random.randrange(1, 999))
+
+a = "Hello, World!"
+print(a.lower())
+
+age = 25
+txt = "My name is Tanveer, and I am {}"
+print(txt.format(age))
+'''
+
+x="Tanveer logavi"
+print(len(x))
+
+
+a= "jamiya masjid near arcot darga bijapur karnataka "
+
+print("j m" in a )
+
+
+
+    
